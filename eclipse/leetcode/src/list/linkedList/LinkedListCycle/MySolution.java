790906eaa6daacfa61f9174096431eeed1666bee
BigBigZe <38675201+BigBigZe@users.noreply.github.com>
2019-04-27 18:58:20 +0800

    141. Linked List Cycle

---
+++ eclipse/leetcode/src/list/linkedList/LinkedListCycle/MySolution.java
@@ -0,0 +1,18 @@
+package list.linkedList.LinkedListCycle;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import list.linkedList.ListNode;
+
+public class MySolution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> s = new HashSet<ListNode>();
+        while(head!=null) {
+        	if(s.contains(head))return true;
+        	s.add(head);
+        	head = head.next;
+        }
+        return false;
+    }
+}
---
+++ eclipse/leetcode/src/list/linkedList/LinkedListCycle/OptimalSolution.java
@@ -0,0 +1,18 @@
+package list.linkedList.LinkedListCycle;
+
+import list.linkedList.ListNode;
+
+public class OptimalSolution {
+	
+    public boolean hasCycle(ListNode head) {
+    	ListNode fast = head;
+    	ListNode slow = head;
+    	while(fast!=null) {
+    		if(fast.next==null||fast.next.next==null)return false;
+    		fast = fast.next.next;
+    		slow = slow.next;
+    		if(fast == slow)return true;
+    	}
+        return false;
+    }
+}
