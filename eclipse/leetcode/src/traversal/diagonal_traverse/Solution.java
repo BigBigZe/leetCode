f7149d0b0b381587f018ac76dd8e498b7ae34003
BigBigZe <572334590@qq.com>
2019-08-30 08:37:08 +0800

    加注释

--- eclipse/leetcode/src/traversal/diagonal_traverse/Solution.java
+++ eclipse/leetcode/src/traversal/diagonal_traverse/Solution.java
@@ -9,20 +9,20 @@ public class Solution {
     	res = new int[m*n];
     	int direction = 0,x=0,y=0,k=0;
     	while(k<res.length){
-    		if(direction == 0) {
+    		if(direction == 0) {//右上方遍历
     			while(x>=0&&y<n) {
     				res[k++] = matrix[x--][y++];
     			}
-    			if(y>=n) {
+    			if(y>=n) {//同时越界，这里只写一个条件是因为y>=n为x<0的充分条件
     				y = n-1;
     				x += 2;
     			}else if(x<0)x=0;
     			direction++;
-    		}else {
+    		}else {//左下方遍历
     			while(x<m&&y>=0) {
     				res[k++] = matrix[x++][y--];
     			}
-    			if(x>=m) {
+    			if(x>=m) {//同时越界
     				x = m-1;
     				y += 2;
     			}else if(y<0)
--- eclipse/leetcode/src/traversal/diagonal_traverse/SubProblem.java
+++ eclipse/leetcode/src/traversal/diagonal_traverse/SubProblem.java
@@ -5,12 +5,15 @@ public class SubProblem {
 		int k=0;
 		int m = nums.length,n=nums[0].length;
 		int[] res = new int[m*n];
+		//先便利第一行开头的所有斜行
 		for(int j=0;j<n;j++) {
 			int x=0,y=j;
 			while(x<m&&y>=0) {
 				res[k++] = nums[x++][y--];
 			}
 		}
+		//再遍历最后一列开头的所有斜行
+		//i=1是因为第一行的最后一个元素和最后一列的第一个元素重复
 		for(int i=1;i<m;i++) {
 			int x=i,y=n-1;
 			while(x<m&&y>=0) {
