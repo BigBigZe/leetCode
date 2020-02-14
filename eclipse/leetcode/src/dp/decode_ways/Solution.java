3df6e15804c5aa52ef81abe24a3ae76603ea03f3
BigBigZe <572334590@qq.com>
2020-02-14 15:20:15 +0800

    91 解码方法

--- eclipse/leetcode/src/dp/decode_ways/Solution.java
+++ eclipse/leetcode/src/dp/decode_ways/Solution.java
@@ -5,34 +5,23 @@ import java.util.List;
 public class Solution {
     public int numDecodings(String s) {
     	int n = s.length();
-    	int[][] dp = new int[n][n];
-    	for(int i=0;i<n-1;i++) {
-    		dp[i][i]=1;
-    		int last = Integer.parseInt(s.substring(i, i+2));
-    		if(last<=26)dp[i][i+1]=2;
+    	int [] dp = new int[n+1];
+    	dp[0] = 1;dp[1]=s.charAt(0) == '0' ? 0 : 1;
+    	for(int i=2;i<n+1;i++) {
+    		int num = Integer.parseInt(s.substring(i-2,i));
+    		if(num<=26&&num>=10)
+    			dp[i] += dp[i-2];
+    		if(s.charAt(i-1)!='0')
+    			dp[i] += dp[i-1];
     	}
-    	dp[n-1][n-1] = 1;
-    	for(int k=1;k<n;k++) {
-    		for(int i=0;i+k<n;i++) {
-    			int max = 0;
-    			max = max>dp[i][i+k]?max:dp[i][i+k];
-    			for(int j=i;j<=i+k;j++) {
-    				max=max>dp[i][j]*dp[j+1][i+k]?max:dp[i][j]*dp[j+1][i+k];
-    			}
-    			dp[i][i+k]=max;
-    		}
-    	}
-    	return dp[0][n-1];
+    	return dp[n];
     }
     
-    public int a(List<Integer> list) {
-    	return 1;
-    }
 
     
     public static void main(String[] args) {
     	
-//		new Solution().numDecodings("262");
+		new Solution().numDecodings("226");
     	
 	}
 }
